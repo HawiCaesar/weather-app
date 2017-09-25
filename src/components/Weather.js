2d9a0d5db9034cc6407af9e64db8e6f832b04ec2
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-25 15:28:31 +0300

    [Feature] Add show weather and show location functions, Add spinner to component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,64 @@
 import React from "react";
+import {CircleLoader} from 'react-spinners';
 
 class Weather extends React.Component {
 
-  render(){
+  show_weather_info(){
 
-    return <p>New</p>;
+    if (this.props.weather_details.fetched_city !=="" && this.props.weather_details.fetched_weather !== false){
+        return (
+          <div>
+            <p>Today's Weather</p>
+          </div>
+
+        );
+    }
+
+   return (
+     <div>
+       <CircleLoader
+         color={'#363CD7'}
+         size={'120'}
+         loading={this.props.loading}
+       />
+       <p>Got Your Location</p>
+     </div>
+   );
+
+  }
+
+  show_location(){
+
+    if (this.props.weather_details.fetched_location === false) {
+
+      return (
+        <CircleLoader
+          color={'#363CD7'}
+          size={'120'}
+          loading={this.props.loading}
+        />
+      );
+    }
+
+    return(
+      <div>
+
+        {this.show_weather_info()}
+
+      </div>
+    );
+
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div className={this.props.styles}>
+          {this.show_location()}
+        </div>
+      </div>
+    );
   }
 
 }
